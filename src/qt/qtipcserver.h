99e19453bbe8d6e5e0055fa38863fd31c674009c
nescoinnet <nescoinnet@gmail.com>
2019-09-23 10:07:09 +0000

    NES commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define NES-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "NESURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
